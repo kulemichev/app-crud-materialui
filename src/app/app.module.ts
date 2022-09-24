8057a192381e73901cb97d50ba017d3bcd76ee05
Alex Kulemichev <alexey.kulemichev@gmail.com>
2022-09-24 11:01:07 +0300

    создали реактивную форму

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
@@ -14,6 +13,7 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatRadioModule} from "@angular/material/radio";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -23,6 +23,7 @@ import {MatRadioModule} from "@angular/material/radio";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-dialog',
@@ -7,16 +8,29 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
+  form: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    productCondition: ['', [Validators.required]],
+    price: ['', [Validators.required]],
+    comment: ['', [Validators.required]],
+    date: ['', [Validators.required]]
+  })
+
   productConditionList = [
     {value: 'new', caption: 'Новый'},
     {value: 'used', caption: 'Б/у'},
-    {value: 'repaired', caption: 'После ремонта'},
-  ]
+    {value: 'repaired', caption: 'После ремонта'}
+  ];
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+  }
+
+  addProduct(): void {
+    console.log(this.form.value);
   }
 
 }
